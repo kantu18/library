b2e6ba9e145ace5c7680dc5718a0065292eda6be
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:05:14 +0530

    use a constructor for the private members

--- src/main/java/com/raj/library/entity/User.java
+++ src/main/java/com/raj/library/entity/User.java
@@ -12,4 +12,13 @@ public class User {
     private int dueBooks;
     private int assignedBooks;
 
+    public User(String userName, String password, String name, int age, String id, int dueBooks, int assignedBooks) {
+        this.userName = userName;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.id = id;
+        this.dueBooks = dueBooks;
+        this.assignedBooks = assignedBooks;
+    }
 }
