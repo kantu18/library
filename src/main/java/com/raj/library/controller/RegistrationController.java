ebbdec3632e09f7c81deb8ef18cc154fdae791e6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 20:08:51 +0530

    create a new method for add a new admin which is mapped by /addadmin and use ModelAttribute for ModelView in spring mvc

--- src/main/java/com/raj/library/controller/RegistrationController.java
+++ src/main/java/com/raj/library/controller/RegistrationController.java
@@ -1,8 +1,11 @@
 package com.raj.library.controller;
 
 import com.raj.library.Service.AdminService;
+import com.raj.library.entity.Admin;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,4 +14,9 @@ public class RegistrationController {
 
     @Autowired
     private AdminService adminService;
+
+    @PostMapping("/addAdmin")
+    public void addAdmin(@ModelAttribute Admin admin){
+
+    }
 }
