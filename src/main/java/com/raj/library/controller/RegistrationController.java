24b81a8093b9f8bb0f44516a90bf8aabb06863a6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:57:20 +0530

    use controller and request mapping annotation for the RegistrationController

--- src/main/java/com/raj/library/controller/RegistrationController.java
+++ src/main/java/com/raj/library/controller/RegistrationController.java
@@ -1,4 +1,10 @@
 package com.raj.library.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registration")
 public class RegistrationController {
+
 }
