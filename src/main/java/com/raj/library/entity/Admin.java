1dd6f790a7a62d92e3e63adbe7c8b1b84257bb46
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:18:24 +0530

    add the admins name, age, phone number, email id etc

--- src/main/java/com/raj/library/entity/Admin.java
+++ src/main/java/com/raj/library/entity/Admin.java
@@ -6,4 +6,8 @@ public class Admin {
     @Id
     private String userName;
     private String password;
+    private String name;
+    private int age;
+    private String phoneNumber;
+    private String emailId;
 }
