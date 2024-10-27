0c046635c8083910ae2146a1e7199f0522b1e98a
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:20:34 +0530

    add the getter and setter method for Admin.java members

--- src/main/java/com/raj/library/entity/Admin.java
+++ src/main/java/com/raj/library/entity/Admin.java
@@ -19,4 +19,52 @@ public class Admin {
         this.phoneNumber = phoneNumber;
         this.emailId = emailId;
     }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 }
