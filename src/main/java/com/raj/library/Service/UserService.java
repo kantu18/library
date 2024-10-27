2162bfa1016880088dafb3da87a8f97d1b1c6df0
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:40:21 +0530

    add the instance of the AdminRepo and use Autowired annotation for the dependency injection

--- src/main/java/com/raj/library/Service/UserService.java
+++ src/main/java/com/raj/library/Service/UserService.java
@@ -1,7 +1,12 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.AdminRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+
+    @Autowired
+    AdminRepo adminRepo;
 }
