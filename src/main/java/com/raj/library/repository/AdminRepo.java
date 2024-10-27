e19738177b85601601af920e7ad70f7f63dd6a1f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:37:37 +0530

    use Repository annotation to tell the spring boot that this is my repository

--- src/main/java/com/raj/library/repository/AdminRepo.java
+++ src/main/java/com/raj/library/repository/AdminRepo.java
@@ -2,6 +2,8 @@ package com.raj.library.repository;
 
 import com.raj.library.entity.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepo extends JpaRepository<Admin,String> {
 }
