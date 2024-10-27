52bcc01f7a18c689063e2a599bd704abfe1fc022
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 20:55:30 +0530

    make a list that stores admins and create a method that return the list of admins

--- src/main/java/com/raj/library/Service/AdminService.java
+++ src/main/java/com/raj/library/Service/AdminService.java
@@ -5,6 +5,9 @@ import com.raj.library.repository.AdminRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class AdminService {
 
@@ -14,5 +17,9 @@ public class AdminService {
 
     public void addAdmin(Admin admin){
         adminRepo.save(admin);
+    }
+    List<Admin> adminList = new ArrayList<>();
+    public List<Admin> getAdmins(){
+
     }
 }
