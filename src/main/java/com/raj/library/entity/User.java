69113a29d191590f0b2c4daf5612fc042ba887c4
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 18:59:07 +0530

    Initial commit

---
+++ src/main/java/com/raj/library/entity/User.java
@@ -0,0 +1,13 @@
+package com.raj.library.entity;
+
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    private String username;
+    private String password;
+    private String name;
+    private int age;
+    private String id;
+    private
+}
