a9693d895c1e130fc66b741486deb795f83d4c5b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:19:06 +0530

    add the parameterized constructor for admin

--- src/main/java/com/raj/library/entity/Admin.java
+++ src/main/java/com/raj/library/entity/Admin.java
@@ -10,4 +10,13 @@ public class Admin {
     private int age;
     private String phoneNumber;
     private String emailId;
+
+    public Admin(String userName, String password, String name, int age, String phoneNumber, String emailId) {
+        this.userName = userName;
+        this.password = password;
+        this.name = name;
+        this.age = age;
+        this.phoneNumber = phoneNumber;
+        this.emailId = emailId;
+    }
 }
