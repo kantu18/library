9f0ab2371e894d52875034605fa54551aa9f0ca6
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:52:34 +0530

    add the AdminService class instance and Autowired it for the dependency injection

--- src/main/java/com/raj/library/controller/LoginController.java
+++ src/main/java/com/raj/library/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,4 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping
 public class LoginController {
 
+    @Autowired
+    AdminService adminService;
 }
