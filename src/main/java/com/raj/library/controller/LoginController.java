bd0ba6d17e05b6c4c2ae03aa8d52c53ba2944c89
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 22:56:26 +0530

    add a method that take the input from the webpage and check the username and password from my database and after that login our admin

--- src/main/java/com/raj/library/controller/LoginController.java
+++ src/main/java/com/raj/library/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.raj.library.controller;
 import com.raj.library.Service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,5 +14,9 @@ public class LoginController {
     @Autowired
     AdminService adminService;
 
+    @GetMapping("/adminLogin")
+    public void adminLogin(Model model){
+
+    }
 
 }
