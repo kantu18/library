d595d7b3a01cf10fdd0f7c8f1daf9bc194947c65
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-27 19:45:19 +0530

    change the name of the class

--- src/main/java/com/raj/library/Service/AdminService.java
+++ src/main/java/com/raj/library/Service/AdminService.java
@@ -5,8 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class UserService {
+public class AdminService {
 
     @Autowired
     AdminRepo adminRepo;
+
 }
