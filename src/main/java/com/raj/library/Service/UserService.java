827c20901666e1b9a69facdbe23d431dfed61c29
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-28 23:40:28 +0530

    add a method that check the username exist in our db or not if yes return true else false

--- src/main/java/com/raj/library/Service/UserService.java
+++ src/main/java/com/raj/library/Service/UserService.java
@@ -1,10 +1,20 @@
 package com.raj.library.Service;
 
+import com.raj.library.entity.Admin;
 import com.raj.library.repository.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    @Autowired
     private UserRepo userRepo;
 
+    public boolean checkUserName(Admin admin){
+        if(userRepo.existsById(admin.getUserName())){
+            return true;
+        }else {
+            return false;
+        }
+    }
 }
