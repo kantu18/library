5fdd0ded7e4c64f840177b8893259cdf28d593be
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-28 23:25:29 +0530

    use repository annotation that is a indentification for spring boot that this is my repository

--- src/main/java/com/raj/library/repository/UserRepo.java
+++ src/main/java/com/raj/library/repository/UserRepo.java
@@ -2,6 +2,8 @@ package com.raj.library.repository;
 
 import com.raj.library.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepo extends JpaRepository<User,String > {
 }
