f9c5b60a8b9357e72e6fb3e0e7f174ca6954b57f
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-28 23:28:26 +0530

    create a private UserRepo instance

--- src/main/java/com/raj/library/Service/UserService.java
+++ src/main/java/com/raj/library/Service/UserService.java
@@ -1,7 +1,10 @@
 package com.raj.library.Service;
 
+import com.raj.library.repository.UserRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepo userRepo;
+
 }
