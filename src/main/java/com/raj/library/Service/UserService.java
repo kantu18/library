a032ef32583025f568cee0ae8855dba2143e74a2
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-10-28 23:43:43 +0530

    add a method that check the password is correct or not from our db or not if yes return true else false

--- src/main/java/com/raj/library/Service/UserService.java
+++ src/main/java/com/raj/library/Service/UserService.java
@@ -1,19 +1,33 @@
 package com.raj.library.Service;
 
 import com.raj.library.entity.Admin;
+import com.raj.library.entity.User;
 import com.raj.library.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepo userRepo;
 
-    public boolean checkUserName(Admin admin){
-        if(userRepo.existsById(admin.getUserName())){
+    public boolean checkUserName(User user){
+        if(userRepo.existsById(user.getUserName())){
             return true;
         }else {
+            return false;
+        }
+    }
+    public boolean checkPassword(User user){
+        Optional<User> byId = userRepo.findById(user.getUserName());
+        System.out.println(byId.get().getPassword());
+        System.out.println(user.getPassword());
+        if(byId.get().getPassword().equals(user.getPassword())){
+            adminUserName = byId.get().getUserName();
+            return true;
+        }else{
             return false;
         }
     }
