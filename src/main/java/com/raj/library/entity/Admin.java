50528ba705f3af55a807264db589f12bbe0b998b
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-02 20:24:36 +0530

    change the table name

--- src/main/java/com/raj/library/entity/Admin.java
+++ src/main/java/com/raj/library/entity/Admin.java
@@ -2,8 +2,10 @@ package com.raj.library.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="Librarian")
 public class Admin {
     @Id
     private String userName;
