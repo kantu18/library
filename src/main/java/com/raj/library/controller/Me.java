206d71868f16fa9103b0c3eb391376cef9c33e91
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-11 15:12:49 +0530

    add a controller layer that mapped with me

---
+++ src/main/java/com/raj/library/controller/Me.java
@@ -0,0 +1,9 @@
+package com.raj.library.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Me")
+public class Me {
+}
