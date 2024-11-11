557fe4cda9109ce6345768cd72649fed1409ff19
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-11 15:16:00 +0530

    add a method of get mapping that fetch the user data from DB and show it in Me.html page

--- src/main/java/com/raj/library/controller/Me.java
+++ src/main/java/com/raj/library/controller/Me.java
@@ -1,9 +1,19 @@
 package com.raj.library.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/Me")
 public class Me {
+
+
+
+    @GetMapping("details")
+    public String getUser(Model model){
+        model.addAttribute("details",userService.getUser());
+        return "Me";
+    }
 }
