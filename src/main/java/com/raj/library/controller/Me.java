497de2b703741f6b0ba3828d39035426859a3619
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-11 15:16:38 +0530

    add the instance of user service class

--- src/main/java/com/raj/library/controller/Me.java
+++ src/main/java/com/raj/library/controller/Me.java
@@ -1,5 +1,7 @@
 package com.raj.library.controller;
 
+import com.raj.library.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/Me")
 public class Me {
 
-
+    @Autowired
+    private UserService userService;
 
     @GetMapping("details")
     public String getUser(Model model){
