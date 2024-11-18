81e944d549fde31fb485dd4b1f10feeb38461a70
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:30:43 +0530

    add the instance of OtpService

--- src/main/java/com/raj/library/controller/OTPController.java
+++ src/main/java/com/raj/library/controller/OTPController.java
@@ -1,10 +1,14 @@
 package com.raj.library.controller;
 
 
+import com.raj.library.Service.OtpService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/Verify")
 public class OTPController {
+    @Autowired
+    private OtpService otpService;
 }
