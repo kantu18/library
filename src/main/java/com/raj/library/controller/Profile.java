1068914f36bfa5ee00ebd70da5ed96d4802a1d10
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 19:45:09 +0530

    change the mapping on Me

--- src/main/java/com/raj/library/controller/Profile.java
+++ src/main/java/com/raj/library/controller/Profile.java
@@ -8,15 +8,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/Me")
-public class Me {
+@RequestMapping("/Profile")
+public class Profile {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping()
+    @GetMapping("/Me")
     public String getUser(Model model){
         model.addAttribute("details",userService.getUser());
         return "Me";
+    }
+    @GetMapping("/Edit")
+    public String EditUser(){
+        return "Edit";
     }
 }
