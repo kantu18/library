d5ea03e30768d09fed1649e4bf0e77a655597377
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:19:34 +0530

    make the Email Service class a service class

--- src/main/java/com/raj/library/Service/EmailService.java
+++ src/main/java/com/raj/library/Service/EmailService.java
@@ -2,7 +2,9 @@ package com.raj.library.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
