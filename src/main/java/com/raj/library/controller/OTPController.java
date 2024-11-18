217b85eced892eb2afa07e9a30bac85737c3ad3c
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:29:04 +0530

    add the OTPController class and make it the Controller and request mapping by verify

---
+++ src/main/java/com/raj/library/controller/OTPController.java
@@ -0,0 +1,10 @@
+package com.raj.library.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/Verify")
+public class OTPController {
+}
