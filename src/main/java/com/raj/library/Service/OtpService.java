4110d0264f2fd5a0e58b82a222d9b7c6eb2f60e4
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:26:11 +0530

    add a method to generate otp by randomness and use for loop to generate 6 digit

--- src/main/java/com/raj/library/Service/OtpService.java
+++ src/main/java/com/raj/library/Service/OtpService.java
@@ -2,7 +2,18 @@ package com.raj.library.Service;
 
 import org.springframework.stereotype.Service;
 
+import java.security.SecureRandom;
+
 @Service
 public class OtpService {
     private static final int otp_Length = 6;
+    public String generateOTP(){
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<otp_Length;i++){
+            sb.append(random.nextInt(10));
+        }
+        return sb.toString();
+    }
 }
