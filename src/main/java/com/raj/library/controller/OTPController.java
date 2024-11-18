807edfeb2420768059ba4a90a447fea58e4c3755
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:42:38 +0530

    send otp to the perticular mail and store it in a hashmap

--- src/main/java/com/raj/library/controller/OTPController.java
+++ src/main/java/com/raj/library/controller/OTPController.java
@@ -3,9 +3,12 @@ package com.raj.library.controller;
 
 import com.raj.library.Service.EmailService;
 import com.raj.library.Service.OtpService;
+import com.raj.library.TempStore.OTPStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/Verify")
@@ -15,4 +18,13 @@ public class OTPController {
 
     @Autowired
     private EmailService emailService;
+
+    @PostMapping("/Verify")
+    public void generateAndSendOTP(@RequestParam("email") String mail){
+        String otp = otpService.generateOTP();
+        System.out.println(otp);
+        emailService.sendOtpEmail(mail,otp);
+        OTPStorage.storeOtp(mail,otp);
+    }
+
 }
