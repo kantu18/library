f20f7c2278041ec5e04dfc2a22bc48dad57110cf
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 20:19:03 +0530

    add the JavaMailSender data type import the class, make it private for security reason and autowired it

--- src/main/java/com/raj/library/Service/EmailService.java
+++ src/main/java/com/raj/library/Service/EmailService.java
@@ -1,4 +1,9 @@
 package com.raj.library.Service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+
 public class EmailService {
+    @Autowired
+    private JavaMailSender mailSender;
 }
