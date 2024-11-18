28232a00a2bd3c13d070bca57ed2275bc5e761e4
Pralayesh Mukherjee <pralayeshmukharjee522@gmail.com>
2024-11-18 23:29:18 +0530

    add a method that delete the user account

--- src/main/java/com/raj/library/controller/Profile.java
+++ src/main/java/com/raj/library/controller/Profile.java
@@ -1,10 +1,13 @@
 package com.raj.library.controller;
 
 import com.raj.library.Service.UserService;
+import com.raj.library.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,7 +23,27 @@ public class Profile {
         return "Me";
     }
     @GetMapping("/Edit")
-    public String EditUser(){
+    public String EditUser(Model model){
+        model.addAttribute("Delete",new User());
         return "Edit";
     }
+    @PostMapping("/deleteAccount")
+    public String editProfile(@ModelAttribute User user){
+        if(userService.checkUserName(user)){
+            System.out.println(user.getUserName());
+            if (userService.checkPassword(user)){
+                System.out.println(user.getPassword());
+                if(userService.deleteUser(user)){
+                    return "DeleteSuccessfully";
+                }else{
+                    return "NotSuccess";
+                }
+            }else{
+                return "wrongPasswordUser";
+            }
+        }else{
+            return "noUser";
+        }
+    }
+
 }
